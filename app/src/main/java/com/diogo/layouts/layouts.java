f1fca963f1deaafc50cdab40cb5db58b9c2cc9f6
olivins <diogo.olivins@gmail.com>
2018-08-24 12:38:38 -0300

    Table Layout, Grid Layout, Scroll View with Grid Layout

---
+++ app/src/main/java/com/diogo/layouts/layouts.java
@@ -0,0 +1,13 @@
+package com.diogo.layouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class layouts extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layouttable);
+    }
+}
